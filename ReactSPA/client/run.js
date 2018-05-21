6edcb74c564343f18d8aaed326721ae34ae3fa97
Diablohu <Diablohu@users.noreply.github.com>
2018-05-21 20:06:21 +0800

    WIP: ReactSPA

--- ReactSPA/client/run.js
+++ ReactSPA/client/run.js
@@ -13,7 +13,7 @@ import thunk from 'redux-thunk'
 import {
     reducer as realtimeLocationReducer,
     REALTIME_LOCATION_REDUCER_NAME,
-    actionUpdate
+    actionUpdate,
 } from 'sp-isomorphic-utils/realtime-location'
 import {
     reducerLocaleId as i18nReducerLocaleId,
@@ -21,8 +21,8 @@ import {
 } from 'super-project/i18n/redux'
 import i18nRegister from 'super-project/i18n/register/spa.client'
 import { ImportStyleRoot } from 'sp-css-import'
-import { onRouterChange } from '@webUI/layout/header'
 
+const ROUTER_REDUCDER_NAME = 'routing'
 
 
 
@@ -32,49 +32,53 @@ export default ({
     redux,
     client
 }) => {
-    // ============================================================================
-    // React 初始化
-    // ============================================================================
-
-    const reactApp = new ReactApp({ rootDom: 'root' })
-
-    reactApp.redux.middleware.use(thunk)
-    reactApp.redux.middleware.use(routerMiddleware(browserHistory))
-    if (__CLIENT__) self.routerHistory = browserHistory
-
-
-
+    const {
+        before,
+        after,
+    } = client
+    const onRouterUpdate = client.routerUpdate || client.onRouterUpdate
+    const onHistoryUpdate = client.historyUpdate || client.onHistoryUpdate
 
     // ============================================================================
     // Redux/Reducer 初始化
     // ============================================================================
 
-    const reducers = {
+    const reducersObject = {
         // 路由状态扩展
         [ROUTER_REDUCDER_NAME]: routerReducer,
         // 目的：新页面请求处理完成后再改变URL
         [REALTIME_LOCATION_REDUCER_NAME]: realtimeLocationReducer,
         // 对应服务器生成的store
-        [SERVER_REDUCER_NAME]: serverReducer,
+        // [SERVER_REDUCER_NAME]: serverReducer,
     }
     if (i18n) {
-        reducers.localeId = i18nReducerLocaleId
-        reducers.locales = i18nReducerLocales
+        reducersObject.localeId = i18nReducerLocaleId
+        reducersObject.locales = i18nReducerLocales
     }
 
     // 兼容配置嵌套
-    if (!redux)
-        redux = client.redux
+    if (!redux) redux = client.redux
 
-    const { combineReducers } = redux
-    if (typeof combineReducers === 'object') {
-        for (let key in combineReducers) {
-            reducers[key] = combineReducers[key]
+    {
+        const { combineReducers } = redux
+        if (typeof combineReducers === 'object') {
+            for (let key in combineReducers) {
+                reducersObject[key] = combineReducers[key]
+            }
         }
     }
-    for (let key in reducers) {
-        reactApp.redux.reducer.use(key, reducers[key])
-    }
+    const reducers = combineReducers(reducersObject)
+    const store = compose(applyMiddleware(thunk))(createStore)(reducers)
+
+
+
+
+
+    // ============================================================================
+    // i18n 初始化
+    // ============================================================================
+    if (i18n) i18nRegister(i18n, store)
+
 
 
 
@@ -88,98 +92,95 @@ export default ({
         else
             router = {}
     }
-    reactApp.react.router.use({
-        path: '',
-        // component: App, 可扩展1层component
-        childRoutes: [router]
+    const routerConfig = {
+        // history: syncHistoryWithStore(memoryHistory, store),
+        history: syncHistoryWithStore(hashHistory, store),
+        routes: router,
+        onUpdate: (...args) => {
+            if (__DEV__)
+                console.log(
+                    `[super/client] ` +
+                    `callback: onRouterUpdate`,
+                    args
+                )
+            // if (__DEV__) console.log('router onUpdate', self.__LATHPATHNAME__, location.pathname)
+            if (typeof onRouterUpdate === 'function')
+                onRouterUpdate(...args)
+        }
+    }
+    // if (__CLIENT__) self.routerHistory = memoryHistory
+    // if (__CLIENT__) self.routerHistory = hashHistory
+
+    // memoryHistory.listen(location => {
+    hashHistory.listen(location => {
+        if (__DEV__) {
+            console.log('🌏 browserHistory update', location)
+        }
+        // console.log(actionUpdate(location))
+        store.dispatch(actionUpdate(location))
+        // console.log(store.getState())
+
+        if (__DEV__)
+            console.log(
+                `[super/client] ` +
+                `callback: onHistoryUpdate`,
+                [location, store]
+            )
+        if (typeof onHistoryUpdate === 'function')
+            onHistoryUpdate(location, store)
     })
 
 
 
 
+
     // ============================================================================
-    // 客户端专用初始化流程
+    // React 初始化
     // ============================================================================
 
-    if (__CLIENT__) {
-        const {
-            before,
-            after,
-        } = client
-        const onRouterUpdate = client.routerUpdate || client.onRouterUpdate
-        const onHistoryUpdate = client.historyUpdate || client.onHistoryUpdate
-
-        reactApp.react.router.ext({
-            onUpdate: (...args) => {
-                if (__DEV__)
-                    console.log(
-                        `[super/client] ` +
-                        `callback: onRouterUpdate`,
-                        args
-                    )
-                // if (__DEV__) console.log('router onUpdate', self.__LATHPATHNAME__, location.pathname)
-                if (typeof onRouterUpdate === 'function')
-                    onRouterUpdate(...args)
-            }
-        })
-
-        if (i18n) i18nRegister(__REDUX_STATE__)
-
-        let beforePromise = before
-        if (__DEV__)
-            console.log(
-                `[super/client] ` +
-                `callback: before`,
-                // args
-            )
-        if (typeof before === 'function') {
-            beforePromise = new Promise(resolve => {
-                before()
-                    .then(() => resolve())
-            })
-        } else if (typeof before !== 'object' || typeof before.then !== 'function') {
-            beforePromise = new Promise(resolve => resolve())
-        }
-
-        beforePromise.then(() =>
-            reactApp.run({
-                browserHistoryOnUpdate: (location, store) => {
-                    // 回调: browserHistoryOnUpdate
-                    // 正常路由跳转时，URL发生变化后瞬间会触发，顺序在react组件读取、渲染之前
-                    if (__DEV__) {
-                        console.log('🌏 browserHistory update', location)
-                    }
-                    // console.log(actionUpdate(location))
-                    store.dispatch(actionUpdate(location))
-                    // console.log(store.getState())
-
-                    if (__DEV__)
-                        console.log(
-                            `[super/client] ` +
-                            `callback: onHistoryUpdate`,
-                            [location, store]
-                        )
-                    if (typeof onHistoryUpdate === 'function')
-                        onHistoryUpdate(location, store)
-                }
-            })
+    let beforePromise = before
+    if (__DEV__)
+        console.log(
+            `[super/client] ` +
+            `callback: before`,
+            // args
         )
-            .then((appData) => {
-                if (__DEV__)
-                    console.log(
-                        `[super/client] ` +
-                        `callback: after`,
-                        [appData]
-                    )
-                if (typeof after === 'function') after(appData)
-            })
+    if (typeof before === 'function') {
+        beforePromise = new Promise(resolve => {
+            before()
+                .then(() => resolve())
+        })
+    } else if (typeof before !== 'object' || typeof before.then !== 'function') {
+        beforePromise = new Promise(resolve => resolve())
     }
 
+    beforePromise
+        .then(() => {
+            const AppWrapper = ImportStyleRoot()(
+                (children) => <div children={children} />
+            )
 
+            ReactDOM.render(
+                <Provider store={store} >
+                    <AppWrapper>
+                        <Router {...routerConfig} />
+                    </AppWrapper>
+                </Provider>,
+                document.getElementById('root')
+            )
 
-
-    // ============================================================================
-    // 结束
-    // ============================================================================
-    return reactApp
+            return true
+        })
+        .then(() => {
+            if (__DEV__)
+                console.log(
+                    `[super/client] ` +
+                    `callback: after`,
+                    [store]
+                )
+            if (typeof after === 'function')
+                after({
+                    store
+                })
+        })
 }
