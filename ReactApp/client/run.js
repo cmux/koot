c73b74862cfcf8b576a64f9e0e0acb2fbfd4ab2c
Diablohu <Diablohu@users.noreply.github.com>
2018-05-29 21:11:51 +0800

    polish

--- ReactApp/client/run.js
+++ ReactApp/client/run.js
@@ -146,9 +146,9 @@ export default ({
                 browserHistoryOnUpdate: (location, store) => {
                     // 回调: browserHistoryOnUpdate
                     // 正常路由跳转时，URL发生变化后瞬间会触发，顺序在react组件读取、渲染之前
-                    if (__DEV__) {
-                        console.log('🌏 browserHistory update', location)
-                    }
+                    // if (__DEV__) {
+                    //     console.log('🌏 browserHistory update', location)
+                    // }
                     // console.log(actionUpdate(location))
                     store.dispatch(actionUpdate(location))
                     // console.log(store.getState())
