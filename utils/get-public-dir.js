8b4b11cf96e0345754cd3172123b5e32d2d1448a
Diablohu <Diablohu@users.noreply.github.com>
2018-06-07 17:20:04 +0800

    优化 git-public-dir

--- utils/get-public-dir.js
+++ utils/get-public-dir.js
@@ -1,8 +1,10 @@
-module.exports = () =>
-    (process.env.WEBPACK_BUILD_ENV === 'dev' || (typeof __DEV__ !== 'undefined' && __DEV__))
+module.exports = () => {
+    const isDev = process.env.WEBPACK_BUILD_ENV === 'dev' || (typeof __DEV__ !== 'undefined' && __DEV__)
+
+    if (process.env.WEBPACK_BUILD_TYPE === 'spa')
+        return isDev ? '/' : ''
+
+    return isDev
         ? `http://localhost:${process.env.WEBPACK_DEV_SERVER_PORT || 3001}/dist/`
-        : (
-            process.env.WEBPACK_BUILD_TYPE === 'spa'
-                ? ''
-                : `/`
-        )
+        : '/'
+}
