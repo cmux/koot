d9e0017b82ee55670105c96ae7450a53a6eff89e
zrain <zrainma@sina.com>
2018-11-09 12:12:54 +0800

    koot-redux v1.0.4

--- packages/koot-redux/lib/redux-module.js
+++ packages/koot-redux/lib/redux-module.js
@@ -9,23 +9,23 @@ class ReduxModule {
     constructor( module, moduleName ) {
 
         const { state, reducers, actions, modules } = module;
-        // 存储初始化的module对象
+        // 
         this.__module__ = module;
-        // 存储 当前模块的  名称
+        //
         this.__moduleName = moduleName;
-        // 存储 当前模块的  reducers 调用对象
+        // 
         this.__reducers = reducers || {};
-        // 存储 当前模块的  state 数据
+        //
         this.__state = state || {};
-        // 存储 当前模块的  初始化state 数据
+        //
         this.__initState = state || {};
-        // 存储 当前模块的  actions 调用对象
+        //
         this.__actions = actions || {};
-        // 存储 当前模块的  子模块初始化module对象
+        //
         this.__modules = modules || {};
-        // 存储 当前模块的  子模块实例化调用对象
+        //         
         this.__children = {};
-        // 存储 当前模块的  外部注入的 reducer 执行序列
+        //
         this.__external_reducers = this.getExternalModuleReducers( this.__state );
 
         // 实例化所有子模块并存储在 __children 下
@@ -39,7 +39,7 @@ class ReduxModule {
     }
 
     /**
-     * 获取当前 state 树上定义的外部注入进来的 reducer 集合
+     * 处理当前 state 树上定义的外部注入进来的 reducer
      * 
      * @param  {[type]} obj  [description]
      * @return {[Array]}     [外部注入进来的reducer的 “特殊对象” 集合]
@@ -64,8 +64,7 @@ class ReduxModule {
     }
 
     /**
-     * 根据对象路径获取对应的值
-     * 
+     * [getDataByPath description]
      * @param  {[type]} obj  [description]
      * @param  {[type]} path [description]
      * @return {[type]}      [description]
@@ -82,8 +81,7 @@ class ReduxModule {
     }
 
     /**
-     * 根据对象路径设置对应的值
-     * 
+     * [setDataByPath description]
      * @param {[type]} obj   [description]
      * @param {[type]} path  [description]
      * @param {[type]} value [description]
@@ -105,7 +103,7 @@ class ReduxModule {
     }
 
     /**
-     * 外部引入reducers集合的统一调用函数
+     * [currentExternalReducerHandler description]
      * 
      * @param  {[type]} moduleState [description]
      * @param  {[type]} action      [description]
@@ -113,28 +111,29 @@ class ReduxModule {
      */
     currentExternalReducerHandler( moduleState, action ) {
         const externalReducers = this.__external_reducers;
-        let finalState;
+        let finalState = moduleState || {};
         externalReducers.forEach(item => {
             const { path, reducer } = item;
-            const currentState = this.getDataByPath(moduleState, path);
+            const currentState = this.getDataByPath(finalState, path);
             const resultState = reducer(
                 (isFunction(currentState) ? undefined : currentState),
                 action
             );
-            finalState = this.setDataByPath(moduleState, path, resultState)
-            finalState = Object.assign({}, moduleState, finalState);
+            const resultModuleState = this.setDataByPath(finalState, path, resultState);
+            finalState = Object.assign({}, finalState, resultModuleState)
         })
-        return finalState || moduleState;
+        return finalState;
     }
 
     /**
-     * 根据 actionName 获取当前 module 的局部 state
+     * [getStateByActionName description]
      * 
      * @param  {[type]} actionName [description]
      * @return {[type]}            [description]
      */
     getStateByActionName( actionName ) {
-        if( this.__actions[actionName] ){
+        const actionCollection = this.actionCollection;
+        if( actionCollection[actionName] ){
             return this.__state;
         }else{
             const childrenModuleNames = Object.keys(this.__children);
@@ -219,33 +218,24 @@ class ReduxModule {
      * @return {[type]} [description]
      */
     createReducer() {
-        // moduleState 默认等于 初始化的state
-        // 如果 执行一个不存在的 action.type 时，会按照默认值来初始化整个状态数
         return ( moduleState = this.__initState, action ) => {
-
             const { type, payload } = action;
-            const reducerHandler = this.__reducers[type];
-            let finaState;
             
+            const reducerHandler = this.__reducers[type];
+
+            let finaState;
+
             // 执行当前 module 里引入的外部 reducer
-            // 因为外部引入的 reducer 结果无法判断是否真正改变了
             // 不论执行结果如何都合并入 finaState 
             const externalStateResult = this.currentExternalReducerHandler(moduleState, action);
-            // 合并结果至 最终的 状态数
             finaState = Object.assign({}, this.__state, moduleState, externalStateResult)
-            // 同步至当前的 __state 
             this.__state = finaState;
-
-            // 如果当前 module 的 reducer 存在则执行 并 同步结果
             if( reducerHandler ){
                 // 执行当前 module 的 reducer
                 const moduleStateResult = reducerHandler(moduleState, payload);
-                // 合并结果至 最终的 状态数
+
                 finaState = Object.assign({}, this.__state, moduleState, finaState, moduleStateResult);
                 this.__state = finaState;
-
-            // 如果当前 module 的 reducer 不存在，
-            // 将局部 state 和 action 传入给 子module的 createReducer 结果处理
             }else{
                 // 把 action 和对应的 state 传给 子元素的 reducer
                 const childrenModuleNames = Object.keys(this.__children);
@@ -259,7 +249,7 @@ class ReduxModule {
                             childrenFinalState[childrenModuleName] = result;
                         }
                     })
-                    // 这里不同步 子模块的执行结果至当前的局部 state 当中
+
                     finaState =  Object.assign({}, this.__state, moduleState, finaState, childrenFinalState);
                 }
             }
