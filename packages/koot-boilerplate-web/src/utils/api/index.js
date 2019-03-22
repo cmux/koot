2b34d083462dd881c6b0a38dd1283f8d9a891ab8
jason-story <jason_story@163.com>
2019-03-22 18:09:31 +0800

    save

---
+++ packages/koot-boilerplate-web/src/utils/api/index.js
@@ -0,0 +1,38 @@
+import path from 'path';
+
+const isDev = __CLIENT__ && !/cheetahgo\.cmcm\.com/.test(window.location.host);
+const apiCtx = require.context('./apis', false, /\.js$/);
+
+export default apiCtx.keys().reduce((exports, file) => {
+    exports[path.basename(file, '.js')] = enhanced(apiCtx(file));
+
+    return exports;
+}, {});
+
+function enhanced(config) {
+    const host = isDev ? config.HOST[0] : config.HOST[1];
+    const createAPI = API =>
+        Object.keys(API).reduce((result, key) => {
+            const pathname = API[key];
+
+            result[key] =
+                typeof pathname === 'string'
+                    ? (...args) => {
+                          let index = 0;
+
+                          return (
+                              host +
+                              pathname.replace(/:[^/]+/gi, match => {
+                                  const arg = args[index++];
+
+                                  return arg === undefined ? match : arg;
+                              })
+                          );
+                      }
+                    : createAPI(pathname);
+
+            return result;
+        }, {});
+
+    return createAPI(config.API);
+}
