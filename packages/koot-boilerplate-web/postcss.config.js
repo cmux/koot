8881613a89af7e2e59c0bcf9babc1b6123f98ffe
jason-story <jason_story@163.com>
2019-03-25 15:01:32 +0800

    save

--- packages/koot-boilerplate-web/postcss.config.js
+++ packages/koot-boilerplate-web/postcss.config.js
@@ -2,13 +2,16 @@ module.exports = {
     plugins: [
         require('autoprefixer'),
         require('cssnano')({
-            preset: ['default', {
-                discardComments: {
-                    removeAll: true
+            preset: [
+                'default',
+                {
+                    discardComments: {
+                        removeAll: true,
+                    },
+                    camelCase: true,
+                    zindex: false,
                 },
-                camelCase: true,
-                zindex: false,
-            }]
-        })
-    ]
-}
+            ],
+        }),
+    ],
+};
