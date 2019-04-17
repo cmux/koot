0284e1794d233bd4f3b8fb50bb375306fb7d9522
Diablohu <Diablohu@users.noreply.github.com>
2019-04-17 16:10:39 +0800

    update test

--- test/cases/react-base/need-in-order.test.js
+++ test/cases/react-base/need-in-order.test.js
@@ -17,6 +17,7 @@ const chalk = require('chalk')
 
 const removeTempProjectConfig = require('../../../packages/koot/libs/remove-temp-project-config')
 const sleep = require('../../../packages/koot/utils/sleep')
+const { styles: puppeteerTestStyles } = require('../puppeteer-test')
 
 //
 
@@ -164,46 +165,8 @@ const doTest = async (port, settings = {}) => {
         await test(2);
     }
 
-    { // 检查 koot-test-styles
-        const tests = await page.evaluate(() => {
-            var hexDigits = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f");
-            //F unction to convert rgb color to hex format
-            function rgb2hex(rgb) {
-                rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-                return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-            }
-            function hex(x) {
-                return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
-            }
-            return Array.from(document.querySelectorAll(`[koot-test-styles]`))
-                .map(el => {
-                    const styles = window.getComputedStyle(el)
-                    const attr = el.getAttribute('koot-test-styles')
-                    const check = {}
-                    const result = {}
-
-                    attr.split(';').forEach(str => {
-                        const [_prop, ..._value] = str.trim().split(':')
-                        const prop = _prop.trim()
-                            .split('-')
-                            .reduce((result, cur) =>
-                                result + cur.substr(0, 1).toUpperCase() + cur.substr(1)
-                            )
-                        check[prop] = _value.join('').trim()
-                        result[prop] = /^rgb\([0-9, ]+?\)$/.test(styles[prop])
-                            ? rgb2hex(styles[prop])
-                            : styles[prop]
-                    })
-
-                    return {
-                        check, result
-                    }
-                })
-        })
-        for (const { check, result } of tests) {
-            expect(check).toEqual(result)
-        }
-    }
+    // 检查 koot-test-styles
+    await puppeteerTestStyles(page)
 
     await browser.close()
 }
