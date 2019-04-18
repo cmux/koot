753f1ab3b9b075c8deabe5135995ffad0583ec91
Diablohu <Diablohu@users.noreply.github.com>
2019-04-18 15:25:19 +0800

    changed all bins' line breaks to LF

--- publish.js
+++ publish.js
@@ -38,9 +38,9 @@ const prePublish = async () => {
     }
     
     // git commit
-    // const git = require('simple-git/promise')(__dirname)
-    // await git.add('./*')
-    // await git.commit(`changed all bins' line breaks to LF`)
+    const git = require('simple-git/promise')(__dirname)
+    await git.add('./*')
+    await git.commit(`changed all bins' line breaks to LF`)
 
     waiting.stop()
     spinner(title).succeed()
