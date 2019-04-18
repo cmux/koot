92df046ade785866630f646e78338c5a30c486e0
Diablohu <Diablohu@users.noreply.github.com>
2019-04-18 14:32:49 +0800

    try fixing #99

--- publish.js
+++ publish.js
@@ -1,11 +1,41 @@
 const fs = require('fs-extra')
 const path = require('path')
 const inquirer = require('inquirer')
+const crlf = require('crlf')
 
 const runScript = require('./libs/run-script')
 const logWelcome = require('./libs/log/welcome')
 const logAbort = require('./libs/log/abort')
 const logFinish = require('./libs/log/finish')
+
+const filesChangedFromCRLFtoLF = []
+
+const prePublish = async () => {
+    const dirKoot = path.resolve(__dirname, './packages/koot')
+    const { bin } = await fs.readJson(path.resolve(dirKoot, 'package.json'))
+    for (const pathname of bin) {
+        const file = path.resolve(dirKoot, pathname)
+        await new Promise((resolve, reject) => {
+            crlf.set(file, 'LF', (err, endingType) => {
+                if (err) return reject(err)
+                if (endingType === 'CRLF')
+                    filesChangedFromCRLFtoLF.push(file)
+                resolve(endingType)
+            })
+        })
+    }
+}
+
+const postPublish = async () => {
+    for (const file of filesChangedFromCRLFtoLF) {
+        await new Promise((resolve, reject) => {
+            crlf.set(file, 'CRLF', (err) => {
+                if (err) return reject(err)
+                resolve()
+            })
+        })
+    }
+}
 
 const run = async () => {
 
@@ -79,13 +109,19 @@ const run = async () => {
         return
     }
 
-    const cmd = `lerna publish`
+    await prePublish()
+    await runScript(
+        `lerna publish`
         + ` --ignore-changes "packages/!(${selected.join('|')})/**"`
         + (tag ? ` --dist-tag ${tag}` : '')
-
-    await runScript(cmd)
+    )
+    await postPublish()
 
     logFinish()
 }
 
-run().catch(e => console.error(e))
+run()
+    .catch(async e => {
+        console.error(e)
+        await postPublish()
+    })
