19cc74aae50d529b0beb8ef4ab2fc02ee072e45d
Diablohu <Diablohu@users.noreply.github.com>
2019-04-19 19:29:29 +0800

    prepare for `bundleVersionsKeep`

---
+++ packages/koot-cli/steps/upgrade/upgrade-from-0.8-to-0.9.js
@@ -0,0 +1,31 @@
+const fs = require('fs-extra')
+const path = require('path')
+
+const spinner = require('../../lib/spinner')
+const _ = require('../../lib/translate')
+const updateVersionInPackagejson = require('./update-version-in-packagejson')
+
+module.exports = async (cwd = process.cwd(), prevVersion = '0.8.0') => {
+
+    const msgUpgrading = require('./msg-upgrading')(prevVersion, '0.9.0')
+    const spinnerUpgrading = spinner(msgUpgrading + '...')
+    const filesChanged = ['package.json']
+
+    // TODO: 添加 config: bundleVersionsKeep = false
+
+    await updateVersionInPackagejson(cwd, '0.9.0')
+
+    // 结束
+    spinnerUpgrading.stop()
+    spinner(msgUpgrading).finish()
+
+    return {
+        // warn: chalk.yellowBright('koot 0.9.0')
+        //     + chalk.reset(' ')
+        //     + '\n'
+        //     + _('upgrade_0.9.0_warning_1')
+        //     + '\n'
+        //     + _('upgrade_0.9.0_warning_2'),
+        files: [...new Set(filesChanged)],
+    }
+}
--- packages/koot/defaults/koot-config.js
+++ packages/koot/defaults/koot-config.js
@@ -18,6 +18,7 @@ module.exports = {
     // serverOnRender: '',
 
     moduleCssFilenameTest: /\.(component|view|module)/,
+    bundleVersionsKeep: 2,
 
     devPort: 3080,
 }
