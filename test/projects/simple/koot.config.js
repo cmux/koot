d8c722d0c3ed125a75eeecc96648d7914dbeeb84
Diablohu <Diablohu@users.noreply.github.com>
2019-05-20 16:21:26 +0800

    update doc

--- test/projects/simple/koot.config.js
+++ test/projects/simple/koot.config.js
@@ -1,12 +1,11 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-
     name: 'Koot Boilerplate',
     template: './src/template.ejs',
     routes: './src/routes',
     redux: {
-        combineReducers: './src/store/reducers',
+        combineReducers: './src/store/reducers'
     },
     aliases: {
         '@src': path.resolve('./src'),
@@ -17,19 +16,29 @@ module.exports = {
         '@store': path.resolve('./src/store'),
         '@views': path.resolve('./src/views'),
         '@server': path.resolve('./server'),
-        "~base.less": path.resolve('./src/constants/less/base.less'),
-        "~Assets": path.resolve('./src/assets'),
-        "~/": path.resolve('./src')
+        '~base.less': path.resolve('./src/constants/less/base.less'),
+        '~Assets': path.resolve('./src/assets'),
+        '~/': path.resolve('./src')
     },
     staticCopyFrom: path.resolve(__dirname, './public'),
 
     port: 8081,
 
     webpackConfig: async () => {
-        const ENV = process.env.WEBPACK_BUILD_ENV
-        if (ENV === 'dev') return await require('./config/webpack/dev')
-        if (ENV === 'prod') return await require('./config/webpack/prod')
-        return {}
+        const ENV = process.env.WEBPACK_BUILD_ENV;
+        if (ENV === 'dev') return await require('./config/webpack/dev');
+        if (ENV === 'prod') return await require('./config/webpack/prod');
+        return {};
+    },
+    webpackAfter: async (kootConfigWithExtra = {}) => {
+        const {
+            __WEBPACK_OUTPUT_PATH,
+            __CLIENT_ROOT_PATH
+        } = kootConfigWithExtra;
+        console.log({
+            __WEBPACK_OUTPUT_PATH,
+            __CLIENT_ROOT_PATH
+        });
     },
     classNameHashLength: 1,
     // bundleVersionsKeep: false,
@@ -43,7 +52,6 @@ module.exports = {
         'react-redux',
         'react-router',
         'react-router-redux',
-        'koot',
-    ],
-
-}
+        'koot'
+    ]
+};
