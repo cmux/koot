88e80ae0e71934089742e237588d29b7cb2975ea
wangyiqi <wangyiqi@cmcm.com>
2019-06-03 19:44:28 +0800

    # echart 相关组件

---
+++ packages/koot-component/components/charts/lib/index.js
@@ -0,0 +1,5 @@
+export { default as transformBarEncode } from './transform-bar-encode.js';
+
+export { default as transformLineEncode } from './transform-line-encode.js';
+
+export { default as transformPieEncode } from './transform-pie-encode.js';
--- packages/koot-component/index.js
+++ packages/koot-component/index.js
@@ -5,3 +5,7 @@ export { default as Nav } from './components/nav';
 export { default as Layout } from './components/layout';
 
 export { default as List } from './components/list';
+
+export { default as Chart } from './components/charts';
+
+export { default as ReactEchart } from './components/react-echarts';
