fa61ef7181087de412ad2b9f549c991ac35be935
Diablohu <Diablohu@users.noreply.github.com>
2019-06-28 15:20:41 +0800

    add more TS definitions

--- packages/koot/index.d.ts
+++ packages/koot/index.d.ts
@@ -1,12 +1,25 @@
 /// <reference path="global.d.ts" />
 
 import { ComponentType, ReactNode, Component } from 'react';
-import { Store, Dispatch } from 'redux';
+import { Store, Dispatch, Middleware, Reducer } from 'redux';
 import { connect } from 'react-redux';
 
 declare module 'koot';
 
-// @extend() ==================================================================
+// general informations =======================================================
+
+/** 获取当前请求对应到项目的语种ID */
+export const getLocaleId: () => LocaleId;
+/** 当前请求对应到项目的语种ID */
+type LocaleId = string;
+
+/** 获取 _Redux store_ 对象 */
+export const getStore: () => Store;
+
+/** 获取封装后的 History 对象 */
+export const getHistory: () => Object;
+
+// HOC extend() ===============================================================
 
 export interface HOCExtend {
     /** React 高阶组件，可赋予目标组件CSS 命名空间、同构数据、更新页面信息等能力。 */
@@ -73,4 +86,20 @@ interface renderProps {
     routes?: Array<Object>;
 }
 
-//
+// for creating store =========================================================
+
+/** 创建 _Redux store_ */
+export const createStore: (
+    appReducer: Reducer | CombineReducerObject,
+    appMiddlewares: Array<Middleware>
+) => Store;
+interface CombineReducerObject {
+    [reducerName: String]: Reducer;
+}
+
+/** 创建 _Redux store_ 时需要用到的内部数据 */
+export interface reduxForCreateStore {
+    reducers: CombineReducerObject;
+    initialState: Object;
+    middlewares: Array<Middleware>;
+}
