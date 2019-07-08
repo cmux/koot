6dc55326d162e03bfd7336907bb6e31980db8886
Diablohu <Diablohu@users.noreply.github.com>
2019-07-08 15:57:44 +0800

    continue fixing pageinfo HOC order problem

--- packages/koot/React/client-update-page-info.js
+++ packages/koot/React/client-update-page-info.js
@@ -46,9 +46,8 @@ export default (title, metas = []) => {
     */
     // 如果当前没有信息，设为当前信息
     if (
-        !infoToChange.title ||
-        !Array.isArray(infoToChange.metas) ||
-        !infoToChange.metas.length
+        !infoToChange.title &&
+        (!Array.isArray(infoToChange.metas) || !infoToChange.metas.length)
     ) {
         infoToChange.title = title;
         infoToChange.metas = metas;
