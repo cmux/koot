d42633fcb6a030bd23de5273cb0d22a89524adf4
Diablohu <diablohu@users.noreply.github.com>
2019-08-17 17:51:47 +0800

    update TS definitions

--- packages/koot/React/component-extender.d.ts
+++ packages/koot/React/component-extender.d.ts
@@ -6,7 +6,7 @@ import { Connect } from 'react-redux';
 
 export interface HOCExtend<ComponentProps> {
     /** React 高阶组件，可赋予目标组件CSS 命名空间、同构数据、更新页面信息等能力。 */
-    <ComponentProps = {}>(options: extendOptions): ExtendComponent<
+    <ComponentProps = {}>(options: ExtendOptions): ExtendComponent<
         ComponentProps
     >;
 }
@@ -22,11 +22,11 @@ interface ExtendComponent<ComponentProps> {
 class HOC extends Component {}
 export const extend: HOCExtend<ComponentProps>;
 
-interface extendOptions {
+interface ExtendOptions {
     connect?: Connect;
     /** 提供页面的 title 和 meta 标签信息 */
-    pageinfo?: extendPageinfoObject | extendPageinfoFunction;
-    data?: extendDataFetch | extendData;
+    pageinfo?: Pageinfo | ExtendPageinfoFunction;
+    data?: DataFetchFunction | ExtendData;
     styles?: KootComponentStyleObject;
     /**
      * 控制 SSR 行为
@@ -37,7 +37,7 @@ interface extendOptions {
     name?: string;
 }
 
-interface extendPageinfoObject {
+interface Pageinfo {
     title?: string;
     metas?: Array<extendPageinfoMeta>;
 }
@@ -46,48 +46,45 @@ interface extendPageinfoMeta {
     [metaKey: string]: string;
 }
 
-function extendPageinfoFunction(
-    state: any,
-    props: P = Object
-): extendPageinfoObject;
+type ExtendPageinfoFunction = (state: any, props: RenderProps) => Pageinfo;
 
-function extendDataFetch(
+type DataFetchFunction = (
     store: Store,
     renderProps: RenderProps,
     dispatch: Dispatch
-): Promise<any>;
+) => Promise<any>;
 
-function extendDataCheck<S = any, R = RenderProps>(
-    state: S,
-    renderProps: R
-): boolean;
+type DataCheckFunction = (state: any, renderProps: RenderProps) => boolean;
 
-interface extendData {
-    fetch?: extendDataFetch;
-    check?: extendDataCheck;
+interface ExtendData {
+    fetch?: DataFetchFunction;
+    check?: DataCheckFunction;
 }
 
 //
 
 /** extend 高阶组件向目标组件注入的 props */
 export interface ExtendedProps {
-    /** 包含父组件传入的 className 和 extend() 高阶组件注入的 className（如果在 extend 高阶组件中传入了 `styles` 属性）*/
+    /** 包含父组件传入的 `className` 和 `extend()` 高阶组件注入的 `className`（如果在 `extend()` 高阶组件中传入了 `styles` 属性）*/
     readonly className?: string;
-    /** extend() 高阶组件注入的 className（如果在 extend 高阶组件中传入了 `styles` 属性）（排除父组件传入的 className）*/
+    /**
+     * `extend()` 高阶组件注入的 `className`
+     * - 不包含父组件传入的 `className`
+     * - 仅在 `extend()` 高阶组件中传入了 `styles` 属性时才会存在
+     */
     readonly 'data-class-name'?: string;
     /**
-     * 可用来手动触发页面信息更新，语法与 connect() 高阶组件的 pageinfo 的函数用法相同
+     * 可用来手动触发页面信息更新，语法与 `connect()` 高阶组件的 `pageinfo` 的函数用法相同
      * - 如果没有传入参数，会使用 `extend()` 高阶组件中的 `pageinfo` 的值
      * - 仅在 `extend()` 高阶组件中使用 `pageinfo` 属性时才会存在
      */
     readonly updatePageinfo?: ExtendedPropsUpdatePageinfo;
 }
+type ExtendedPropsUpdatePageinfo = (
+    newPageinfo?: Pageinfo | ExtendPageinfoFunction
+) => void;
 
-function ExtendedPropsUpdatePageinfo(
-    newPageinfo?: extendPageinfoObject | extendPageinfoFunction
-): void;
-
-interface RenderProps {
+interface RenderProps extends ExtendedProps {
     readonly location?: Object;
     readonly params?: Object;
     readonly route?: Object;
