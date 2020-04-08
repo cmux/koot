cbe825997b65600a382e98fa763eff6cc355ac83
Diablohu <Diablohu@users.noreply.github.com>
2020-04-08 16:57:19 +0800

    changed all bins' line breaks to LF

--- packages/koot/bin/dev.js
+++ packages/koot/bin/dev.js
@@ -17,7 +17,7 @@ const {
     keyFileProjectConfigTempPortionClient,
     keyFileProjectConfigTempPortionOtherClient,
     filenameWebpackDevServerPortTemp,
-    filenameBuilding,
+    filenameBuilding
     // filenameBuildFail,
     // filenameDll, filenameDllManifest,
 } = require('../defaults/before-build');
@@ -106,14 +106,14 @@ const run = async () => {
         port,
         dll = true,
         kootTest = false,
-        kootDevelopment = false,
+        kootDevelopment = false
     } = program;
 
     initNodeEnv();
     setEnvFromCommand({
         config,
         type,
-        port,
+        port
     });
 
     const stageFromCommand = (() => {
@@ -134,9 +134,9 @@ const run = async () => {
             {
                 message: __('dev.ask_for_auto_open'),
                 suffix: __(`dev.ask_for_auto_open_suffix`, {
-                    seconds: timeout / 1000,
+                    seconds: timeout / 1000
                 }),
-                default: true,
+                default: true
             },
             timeout
         );
@@ -182,7 +182,7 @@ const run = async () => {
         [keyFileProjectConfigTempFull]: fileProjectConfigTempFull,
         [keyFileProjectConfigTempPortionServer]: fileProjectConfigTempPortionServer,
         [keyFileProjectConfigTempPortionClient]: fileProjectConfigTempPortionClient,
-        [keyFileProjectConfigTempPortionOtherClient]: fileProjectConfigTempPortionOtherClient,
+        [keyFileProjectConfigTempPortionOtherClient]: fileProjectConfigTempPortionOtherClient
     } = kootConfig;
     const [devMemoryAllocationClient, devMemoryAllocationServer] = (() => {
         const { devMemoryAllocation } = kootConfig;
@@ -282,7 +282,7 @@ const run = async () => {
             //             ) +
             //             '\n\n'
             //     );
-            const kill = (p) => {
+            const kill = p => {
                 // if (!silent) {
                 //     console.log(
                 //         `TERMINATING: ${p.pm2_env.pm_id} | ${p.pid} | ${p.name}`
@@ -324,7 +324,7 @@ const run = async () => {
                 removeTempBuild(dist),
                 fs.emptyDir(getDirDevTmp(cwd)),
                 // 清理临时目录
-                fs.remove(getDirTemp()),
+                fs.remove(getDirTemp())
             ]);
         } catch (e) {}
 
@@ -404,7 +404,7 @@ const run = async () => {
                 result.errors.length
             ) {
                 error = result.errors;
-                result.errors.forEach((e) => console.error(e));
+                result.errors.forEach(e => console.error(e));
             } else {
                 error = e;
                 console.error(e);
@@ -431,7 +431,7 @@ const run = async () => {
     if (stage) {
         const cmd = `koot-build --stage ${stage} ${buildCmdArgs}`;
         const child = npmRunScript(cmd, {});
-        child.once('error', (error) => {
+        child.once('error', error => {
             // eslint-disable-next-line no-console
             console.trace(error);
             process.exit(1);
@@ -446,7 +446,7 @@ const run = async () => {
             // 等待 filenameBuilding 文件删除
             let flagCreated = false;
             const fileFlagBuilding = path.resolve(dist, filenameBuilding);
-            await new Promise((resolve) => {
+            await new Promise(resolve => {
                 const wait = () =>
                     setTimeout(() => {
                         if (!flagCreated) {
@@ -461,7 +461,7 @@ const run = async () => {
 
             // console.log(' ')
 
-            await new Promise((resolve) => {
+            await new Promise(resolve => {
                 setTimeout(() => {
                     log('success', 'dev', __('dev.spa_success'));
                     // eslint-disable-next-line no-console
@@ -489,7 +489,7 @@ const run = async () => {
 
     // ========================================================================
     //
-    // 没有设置 STAGE，表示同构项目的完整开发环境，开启多个进程
+    // 没有设置 STAGE，表示同构项目的完整开发环��，开启多个进程
     //
     // ========================================================================
     // spinner(
@@ -506,7 +506,7 @@ const run = async () => {
     const pathServerStartFlag = getPathnameDevServerStart();
 
     // 根据 stage 开启 PM2 进程
-    const start = (stage) =>
+    const start = stage =>
         new Promise(async (resolve, reject) => {
             // console.log(`starting ${stage}`)
 
@@ -527,7 +527,7 @@ const run = async () => {
                 cwd: cwd,
                 output: pathLogOut,
                 error: pathLogErr,
-                autorestart: true,
+                autorestart: true
             };
 
             switch (stage) {
@@ -549,12 +549,12 @@ const run = async () => {
                             '.server-start',
                             'node_modules',
                             config.output,
-                            config.error,
+                            config.error
                         ],
                         watch_options: {
-                            cwd: path.dirname(pathServerJS),
+                            cwd: path.dirname(pathServerJS)
                             // usePolling: true
-                        },
+                        }
                         // autorestart: true,
                     });
                     // console.log(config);
@@ -576,7 +576,7 @@ const run = async () => {
                     );
                     Object.assign(config, {
                         script: mainScript,
-                        watch: path.dirname(mainScript),
+                        watch: path.dirname(mainScript)
                         // env: {
                         //     DEBUG: 'koa-mount'
                         // }
@@ -593,11 +593,11 @@ const run = async () => {
             pm2.start(config, (err, proc) => {
                 // console.log(err)
                 if (err) return reject(err);
-                proc.forEach((p) => {
+                proc.forEach(p => {
                     processes.push({
                         ...p,
                         name: config.name,
-                        pid: p.pid || p.process.pid,
+                        pid: p.pid || p.process.pid
                     });
                 });
                 // console.log(JSON.stringify(proc))
@@ -615,7 +615,7 @@ const run = async () => {
     };
 
     // 遇到错误
-    const encounterError = (e) => {
+    const encounterError = e => {
         const error = e instanceof Error ? e : new Error(e);
         exitHandler({ error: true });
         throw error;
@@ -624,7 +624,7 @@ const run = async () => {
     // 连接 PM2
     // console.log('noDaemon', !global)
     try {
-        pm2.connect(!global, async (err) => {
+        pm2.connect(!global, async err => {
             if (err) {
                 // console.error(err)
                 process.exit(2);
@@ -637,7 +637,7 @@ const run = async () => {
                     __('build.build_start', {
                         type: chalk.cyanBright(__(`appType.${appType}`)),
                         stage: chalk.green('client'),
-                        env: chalk.green('dev'),
+                        env: chalk.green('dev')
                     })
             );
 
@@ -670,7 +670,7 @@ const run = async () => {
                     __('build.build_complete', {
                         type: chalk.cyanBright(__(`appType.${appType}`)),
                         stage: chalk.green('client'),
-                        env: chalk.green('dev'),
+                        env: chalk.green('dev')
                     })
             );
             // console.log(processClient[0].process, processClient[0].pid)
@@ -694,7 +694,7 @@ const run = async () => {
                     __('build.build_start', {
                         type: chalk.cyanBright(__(`appType.${appType}`)),
                         stage: chalk.green('server'),
-                        env: chalk.green('dev'),
+                        env: chalk.green('dev')
                     })
             );
             await start('server');
@@ -717,7 +717,7 @@ const run = async () => {
                     __('build.build_complete', {
                         type: chalk.cyanBright(__(`appType.${appType}`)),
                         stage: chalk.green('server'),
-                        env: chalk.green('dev'),
+                        env: chalk.green('dev')
                     })
             );
 
@@ -760,7 +760,7 @@ const run = async () => {
                 // eslint-disable-next-line no-console
                 console.log(' ');
                 return await exitHandler({
-                    silent: true,
+                    silent: true
                 });
             }
 
@@ -778,6 +778,6 @@ const openBrowserPage = () => {
     return opn(`http://localhost:${process.env.SERVER_PORT}/`);
 };
 
-run().catch((err) => {
+run().catch(err => {
     console.error(err);
 });
