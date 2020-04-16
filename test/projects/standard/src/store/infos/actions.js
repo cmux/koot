e3b8f5757d1373e553ab23958372424383f1b046
Diablohu <Diablohu@users.noreply.github.com>
2020-04-16 17:47:38 +0800

    update test project

--- test/projects/standard/src/store/infos/actions.js
+++ test/projects/standard/src/store/infos/actions.js
@@ -7,26 +7,28 @@ const apiBase = `http://127.0.0.1:${getPort()}`;
 export const updateServerTimestamp = () => (dispatch) => {
     // console.log((typeof Store === 'undefined' ? `\x1b[31m×\x1b[0m` : `\x1b[32m√\x1b[0m`) + ' Store in [action] updateServerTimestamp')
 
-    return dispatch({
-        type: SET_DATA_TS,
-        ts: Date.now(),
-    });
-    // return axios({
-    //     url: `${apiBase}/api/json-test`,
-    //     method: 'GET'
-    // })
-    //     .then(res => {
-    //         if (typeof res !== 'object') throw new Error('REQUEST_FAIL:UNKNOWN')
-    //         if (res.status != 200) throw new Error(`REQUEST_FAIL:STATUS:${res.status}`)
-    //         if (res.statusText != 'OK') throw new Error(`REQUEST_FAIL:${res.statusText}`)
-    //         if (typeof res.data !== 'object') throw new Error(`REQUEST_FAIL:NO_DATA`)
+    // return dispatch({
+    //     type: SET_DATA_TS,
+    //     ts: Date.now(),
+    // });
+    return axios({
+        url: `${apiBase}/api/json-test`,
+        method: 'GET',
+    }).then((res) => {
+        if (typeof res !== 'object') throw new Error('REQUEST_FAIL:UNKNOWN');
+        if (res.status != 200)
+            throw new Error(`REQUEST_FAIL:STATUS:${res.status}`);
+        if (res.statusText != 'OK')
+            throw new Error(`REQUEST_FAIL:${res.statusText}`);
+        if (typeof res.data !== 'object')
+            throw new Error(`REQUEST_FAIL:NO_DATA`);
 
-    //         dispatch({
-    //             type: SET_DATA_TS,
-    //             ts: res.data.current_timestamp
-    //         })
-    //         return res
-    //     })
+        dispatch({
+            type: SET_DATA_TS,
+            ts: res.data.current_timestamp,
+        });
+        return res;
+    });
 };
 
 export const resetServerTimestamp = () => (dispatch) =>
