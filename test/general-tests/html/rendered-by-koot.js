bf0c5bd36835e525e998301d1c09624e91b49f4b
Diablohu <Diablohu@users.noreply.github.com>
2021-01-25 10:37:45 +0800

    chore(unit-test): minor update

--- test/general-tests/html/rendered-by-koot.js
+++ test/general-tests/html/rendered-by-koot.js
@@ -3,18 +3,20 @@ const path = require('path');
 
 // 测试：koot.js 版本信息
 // <!-- rendered by using koot.js 0.10.0-alpha.4 -->
-module.exports = async html => {
+module.exports = async (html) => {
     const { version } = await fs.readJson(
         path.resolve(__dirname, '../../../packages/koot/package.json')
     );
+    const checkStr = `<!-- rendered by using koot.js ${version} -->`;
+    const result = html.includes(checkStr);
 
-    const regex = new RegExp(
-        `\\<\\!-- rendered by using koot\\.js ${version.replace(
-            /\./g,
-            '\\.'
-        )} --\\>`
-    );
+    // const regex = new RegExp(
+    //     `\\<\\!-- rendered by using koot\\.js ${version.replace(
+    //         /\./g,
+    //         '\\.'
+    //     )} --\\>`
+    // );
 
-    if (!regex.test(html)) console.warn(version, regex);
-    expect(regex.test(html)).toBe(true);
+    if (!result) console.warn(version, checkStr);
+    expect(result).toBe(true);
 };
