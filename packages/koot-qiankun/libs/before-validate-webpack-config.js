abb64d6a729df22d5bf66227075edfae5d25b55c
Diablohu <Diablohu@users.noreply.github.com>
2022-07-16 00:11:58 +0800

    fix(qiankun): fix for production bundle

---
+++ packages/koot-qiankun/libs/before-validate-webpack-config.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-console */
+
+const __ = require('./translate');
+
+const defaultQiankunConfig = require('./defaults/qiankun-config');
+
+// ============================================================================
+
+const modifyConfig = async (appConfig) => {
+    // if (appConfig[keyConfigBuildDll]) return;
+
+    if (typeof appConfig !== 'object')
+        throw new Error('MISSING_PARAMETER: appConfig');
+
+    // ========================================================================
+    //
+    // 生成 Qiankun 配置对象
+    //
+    // ========================================================================
+    const qiankunConfig = {
+        ...(appConfig.qiankun || {}),
+        ...defaultQiankunConfig,
+    };
+    if (!qiankunConfig.name)
+        throw new Error(__('MISSING_CONFIG: `qiankun.name`'));
+    if (qiankunConfig.basename && !process.env.KOOT_HISTORY_BASENAME) {
+        process.env.KOOT_HISTORY_BASENAME = qiankunConfig.basename;
+    }
+
+    appConfig.qiankunConfig = qiankunConfig;
+
+    return appConfig;
+};
+
+module.exports = modifyConfig;
